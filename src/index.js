a9fe4313f0e100556d5121502c1712776f6d5ad6
arialghifari <arialghifari99@gmail.com>
2022-09-05 00:02:15 +0800

    Add off-canvas menu

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './styles/main.css';
+
+const btnHamburger = document.querySelector('#hamburger');
+const nav = document.querySelector('.nav');
+
+btnHamburger.addEventListener('click', (e) => {
+  nav.classList.toggle('open');
+  e.stopPropagation();
+});
+
+document.body.addEventListener('click', (e) => {
+  nav.classList.toggle('open');
+  e.stopPropagation();
+});
